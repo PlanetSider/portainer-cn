a509c26337d4c662642537d36287d1a97af241ae
PlanetSider <PlanetSider@qq.com>
2026-04-26 22:03:18 +0800

    sync portainer version with upstream release
    Ultraworked with [Sisyphus](https://github.com/code-yeongyu/oh-my-openagent)
    
    Co-authored-by: Sisyphus <clio-agent@sisyphuslabs.ai>

--- api/portainer.go
+++ api/portainer.go
@@ -1935,9 +1935,9 @@ type (
 
 const (
 	// APIVersion is the version number of the Portainer API
-	APIVersion = "2.40.0"
+	APIVersion = "2.39.1"
 	// Support annotation for the API version ("STS" for Short-Term Support or "LTS" for Long-Term Support)
-	APIVersionSupport = "STS"
+	APIVersionSupport = "LTS"
 	// Edition is what this edition of Portainer is called
 	Edition = PortainerCE
 	// ComposeSyntaxMaxVersion is a maximum supported version of the docker compose syntax
