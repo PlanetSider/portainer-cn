91db31d1530707310c5dcb4403b370bc31d47c61
PlanetSider <PlanetSider@qq.com>
2026-04-26 21:07:47 +0800

    localize container base form labels
    Ultraworked with [Sisyphus](https://github.com/code-yeongyu/oh-my-openagent)
    
    Co-authored-by: Sisyphus <clio-agent@sisyphuslabs.ai>

--- app/react/docker/containers/CreateView/BaseForm/BaseForm.tsx
+++ app/react/docker/containers/CreateView/BaseForm/BaseForm.tsx
@@ -96,8 +96,8 @@ export function BaseForm({
             <div className="form-group">
               <div className="col-sm-12">
                 <SwitchField
-                  label="Always pull the image"
-                  tooltip="When enabled, Portainer will automatically try to pull the specified image before creating the container."
+                  label="总是拉取镜像"
+                  tooltip="启用后，Portainer 会在创建容器前自动尝试拉取指定镜像。"
                   checked={values.alwaysPull}
                   onChange={(alwaysPull) =>
                     setFieldValue('alwaysPull', alwaysPull)
@@ -112,13 +112,13 @@ export function BaseForm({
 
         {canUseWebhook && (
           <Authorized authorizations="PortainerWebhookCreate" adminOnlyCE>
-            <FormSection title="Webhook">
+          <FormSection title="Webhook 回调">
               <div className="form-group">
                 <div className="col-sm-12">
                   <SwitchField
-                    label="Create a container webhook"
+                    label="创建容器 Webhook"
                     data-cy="container-webhook-switch"
-                    tooltip="Create a webhook (or callback URI) to automate the recreate this container. Sending a POST request to this callback URI (without requiring any authentication) will pull the most up-to-date version of the associated image and recreate this container."
+                    tooltip="创建一个 Webhook（或回调 URI）以自动重建此容器。向该回调 URI 发送 POST 请求（无需认证）后，会拉取关联镜像的最新版本并重新创建此容器。"
                     checked={values.enableWebhook}
                     onChange={(enableWebhook) =>
                       setFieldValue('enableWebhook', enableWebhook)
@@ -132,13 +132,13 @@ export function BaseForm({
           </Authorized>
         )}
 
-        <FormSection title="Network ports configuration">
+        <FormSection title="网络端口配置">
           <div className="form-group">
             <div className="col-sm-12">
               <SwitchField
-                label="Publish all exposed ports to random host ports"
+                label="将所有暴露端口发布到随机主机端口"
                 data-cy="publish-all-ports-switch"
-                tooltip="When enabled, Portainer will let Docker automatically map a random port on the host to each one defined in the image Dockerfile."
+                tooltip="启用后，Portainer 会让 Docker 自动将主机上的随机端口映射到镜像 Dockerfile 中定义的每个暴露端口。"
                 checked={values.publishAllPorts}
                 onChange={(publishAllPorts) =>
                   setFieldValue('publishAllPorts', publishAllPorts)
@@ -156,7 +156,7 @@ export function BaseForm({
         </FormSection>
 
         {isAgentOnSwarm && (
-          <FormSection title="Deployment">
+          <FormSection title="部署">
             <NodeSelector
               value={values.nodeName}
               onChange={(nodeName) => setFieldValue('nodeName', nodeName)}
@@ -176,9 +176,9 @@ export function BaseForm({
         <div className="form-group">
           <div className="col-sm-12">
             <SwitchField
-              label="Auto remove"
+              label="自动移除"
               data-cy="container-auto-remove-switch"
-              tooltip="When enabled, Portainer will automatically remove the container when it exits. This is useful when you want to use the container only once."
+              tooltip="启用后，Portainer 会在容器退出时自动将其删除。这适用于只需临时运行一次容器的场景。"
               checked={values.autoRemove}
               onChange={(autoRemove) => setFieldValue('autoRemove', autoRemove)}
               labelClass="col-sm-3 col-lg-2"
@@ -189,12 +189,12 @@ export function BaseForm({
         <div className="form-group">
           <div className="col-sm-12">
             <LoadingButton
-              loadingText="Deployment in progress..."
+              loadingText="部署进行中..."
               data-cy="deploy-container-button"
               isLoading={isLoading}
               disabled={!isValid}
             >
-              Deploy the container
+              部署容器
             </LoadingButton>
           </div>
         </div>
